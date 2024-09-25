e96d406afe97435c5a4704e732e74395ea87e2e1
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2024-09-25 19:53:58 +0100

    Fix address

--- src/components/viewShopPages/aboutShop.tsx
+++ src/components/viewShopPages/aboutShop.tsx
@@ -1,35 +1,44 @@
 import { shopType } from "@/stores/for-customer-store";
 import React from "react";
 
-export default function AboutShop({shop}:{shop:shopType}) {
-    return(
-        <div className="flex items-start justify-between">
-            <div className="flex flex-col gap-y-5 mt-4 justify-start items-start">
-              <div className="text-lg">About {shop.shopName}</div>
-              {shop.address && (
-                <div className="text-xs">
-                  <span className="font-semibold">Address: <br /> </span>
-                  {shop.address.addressLine1 && <span>{shop.address.addressLine1}, </span>} 
-                  {shop.address.addressLine2 && <span>{shop.address.addressLine2}, </span>} 
-                  {shop.address.state && <span>{shop.address.state}, </span>} 
-                  {shop.address.city && <span>{shop.address.city}, </span>} 
-                  {shop.address.postcode && <span>{shop.address.postcode} </span>}
-                </div>
+export default function AboutShop({ shop }: { shop: shopType }) {
+  return (
+    <div className="flex flex-col items-start justify-start max-w-[500px] ">
+      <div className="text-lg mb-5">About {shop.shopName}</div>
+      <div className="flex items-start justify-between gap-x-5">
+        <div className="flex flex-col gap-y-5 justify-start items-start max-w-1/2">
+          {shop.address && (
+            <div className="text-xs">
+              <span className="font-semibold">
+                Address: <br />{" "}
+              </span>
+              {shop.address.addressLine1 && (
+                <span>{shop.address.addressLine1}, </span>
               )}
-              {shop.phone && (
-                <div className="text-xs">
-                  <span className="font-semibold">Phone: </span>
-                  {shop.phone}
-                </div>
+              {shop.address.addressLine2 && (
+                <span>{shop.address.addressLine2}, </span>
               )}
-              {shop.about && (
-                <div className="text-xs">
-                  <span className="font-semibold">About our café: </span>
-                  {shop.about}
-                </div>
-              )}
+              {shop.address.state && <span>{shop.address.state}, </span>}
+              {shop.address.city && <span>{shop.address.city}, </span>}
+              {shop.address.postcode && <span>{shop.address.postcode} </span>}
             </div>
-
+          )}
+          {shop.phone && (
+            <div className="text-xs">
+              <span className="font-semibold">Phone: </span>
+              {shop.phone}
+            </div>
+          )}
+          {shop.about && (
+            <div className="text-xs">
+              <span className="font-semibold">About our café: </span>
+              {shop.about}
+            </div>
+          )}
         </div>
-    )
+        {/* <iframe className="w-1/2 rounded-lg" src="https://www.google.com/maps/embed/v1/place?q=Park%20Rd,%20London%20NW8%207JD" style={{border:0}}  loading="lazy"></iframe> */}
+      </div>
+      
+    </div>
+  );
 }
